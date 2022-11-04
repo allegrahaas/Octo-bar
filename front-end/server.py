54c5bbf7d874a3a6b0239df63f3b6dc59600540c
Alex Quin <alex.j.quin@outlook.com>
2022-11-04 13:47:34 -0700

    Comment cleanup

--- front-end/server.py
+++ front-end/server.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
-##CONNECT TO DB
+# CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///barbot.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
