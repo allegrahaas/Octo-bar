2244c1a5069547e37a4396d3f0f8c11cb2ab2bdd
Alex Quin <alex.j.quin@outlook.com>
2022-11-04 15:36:15 -0700

    Refactor table models to separate file

--- front-end/server.py
+++ front-end/server.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import relationship
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
+from tables import Recipe, Ingredient, db
 import pandas
 
 app = Flask(__name__)
@@ -16,43 +17,7 @@ Bootstrap(app)
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///barbot.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 db.init_app(app)
-
-
-class Recipe(db.Model):
-    __tablename__ = "recipes"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), unique=True, nullable=False)
-    custom = db.Column(db.Boolean, nullable=False)
-
-    ingredient_1_id = db.Column(db.Integer, ForeignKey("ingredients.id"))
-    ingredient_2_id = db.Column(db.Integer, ForeignKey("ingredients.id"), nullable=True)
-    ingredient_3_id = db.Column(db.Integer, ForeignKey("ingredients.id"), nullable=True)
-    ingredient_4_id = db.Column(db.Integer, ForeignKey("ingredients.id"), nullable=True)
-    ingredient_5_id = db.Column(db.Integer, ForeignKey("ingredients.id"), nullable=True)
-    ingredient_6_id = db.Column(db.Integer, ForeignKey("ingredients.id"), nullable=True)
-
-    amount_1 = db.Column(db.String(10), nullable=False)
-    amount_2 = db.Column(db.String(10), nullable=True)
-    amount_3 = db.Column(db.String(10), nullable=True)
-    amount_4 = db.Column(db.String(10), nullable=True)
-    amount_5 = db.Column(db.String(10), nullable=True)
-    amount_6 = db.Column(db.String(10), nullable=True)
-
-    ingredient_1 = relationship("Ingredient", lazy=False, foreign_keys=[ingredient_1_id])
-    ingredient_2 = relationship("Ingredient", lazy=False, foreign_keys=[ingredient_2_id])
-    ingredient_3 = relationship("Ingredient", lazy=False, foreign_keys=[ingredient_3_id])
-    ingredient_4 = relationship("Ingredient", lazy=False, foreign_keys=[ingredient_4_id])
-    ingredient_5 = relationship("Ingredient", lazy=False, foreign_keys=[ingredient_5_id])
-    ingredient_6 = relationship("Ingredient", lazy=False, foreign_keys=[ingredient_6_id])
-
-
-class Ingredient(db.Model):
-    __tablename__ = "ingredients"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), unique=True, nullable=False)
-    location = db.Column(db.String(30), nullable=False)
 
 
 @app.route("/")
