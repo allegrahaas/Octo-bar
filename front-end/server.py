505f348a3f5693c2e2b5474a1741b634eed37fe1
Alex Quin <alex.j.quin@outlook.com>
2022-11-04 13:48:29 -0700

    Add location to Ingredient table

--- front-end/server.py
+++ front-end/server.py
@@ -52,9 +52,7 @@ class Ingredient(db.Model):
     __tablename__ = "ingredients"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
-
-
-ingredients = None
+    location = db.Column(db.String(30), nullable=False)
 
 
 @app.route("/")
